ec67adba3b7d5e34e998a93353dc259576e60c77
IsmailWebDev <ismail.racaf.official@gmail.com>
2023-10-26 16:55:38 +0100

    chore: add image links

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "www.cdestarreja.pt",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
