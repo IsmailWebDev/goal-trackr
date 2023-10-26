94ec4add2ff4ea314c8590e8f8b262b7cc186aa8
IsmailWebDev <ismail.racaf.official@gmail.com>
2023-10-26 16:56:12 +0100

    feat: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        main: '#333533',
+        card: '#202020',
+      },
+
+      colors: {
+        goal: '#20bf55',
+        stat: '#a4161a',
+        card: '#d6d6d6',
+        season: '#133c55',
+        winning_goal: '#e9c46a',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
